5fa73c051854c78eb74891e76c5c120de32179c6
Ashby Blakely <ashbyleeblakely@gmail.com>
2021-10-19 16:17:19 -0400

    added some styling to the header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Nav from './components/Nav';
 import Header from './components/Header';
 import Project from './components/Project';
 import Footer from "./components/Footer";
@@ -8,7 +7,6 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <div>
-      <Nav/>
       <Header/>
       <Project/>
       <Footer/>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,41 @@
 import React from 'react';
 
 const styles = {
-  headerStyle: {
-    background: 'red',
-  },
-  headingStyle: {
-    fontSize: '100px',
-  },
+    headerStyle: {
+        background: 'red',
+
+    },
+    headingStyle: {
+        fontSize: '75',
+    },
+    navStyle: {
+        color: 'black',
+        padding: '20px',
+    },
+    wordStyle: {
+        padding: '10px',
+    }
 };
 
 
 function Header() {
-  return (
-    <header style={styles.headerStyle} className="header">
-      <h1 style={styles.headingStyle}>Welcome</h1>
-    </header>
-  );
+    return (
+        <div>
+            <div class="d-flex align-items-center">
+                <header class="d-flex" style={styles.headerStyle} className="header">
+                    <h1 style={styles.headingStyle}>Ashby Blakely</h1>
+                    <div class="d-flex justify-content-end">
+                        <nav style={styles.navStyle} className="navbar">
+                            <a style={styles.wordStyle} href="/">About Me</a>
+                            <a style={styles.wordStyle} href="/">Projects</a>
+                            <a style={styles.wordStyle} href="/">Contact</a>
+                            <a style={styles.wordStyle} href="/">Resume</a>
+                        </nav>
+                    </div>
+                </header>
+            </div>
+        </div>
+    );
 }
 
 export default Header;
