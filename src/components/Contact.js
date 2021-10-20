6c2e9f1c9718c226644cdb08312b5c8815adbae1
Ashby Blakely <ashbyleeblakely@gmail.com>
2021-10-20 08:39:11 -0400

    added a form to the contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,19 @@ import React from 'react';
 
 function Contact() {
     return (
-        <div class="d-flex justify-content-center">
-            <h1>Footer</h1>
-        </div>
+        <form className="container col-3 text-white text-center pt-5">
+            <div class="mb-3">
+                <h3>Contact Me!</h3>
+                <label for="exampleInputEmail1" className="form-label">Email address</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div className="mb-3">
+                <label for="exampleInputPassword1" className="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" />
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
     );
 }
 
