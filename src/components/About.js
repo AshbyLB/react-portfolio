98e5fc2856074509256050a4e3e03067b0c39eae
Ashby Blakely <ashbyleeblakely@gmail.com>
2021-11-01 15:36:59 -0400

    changed about me text

--- src/components/About.js
+++ src/components/About.js
@@ -9,9 +9,9 @@ function About() {
             <div id="aboutStyle">
                 <h2>About Me</h2>
                 <img id="me" src={Ashby} alt="Photo of Ashby in Ireland" />
-                <p>I'm a current student of the UNCC Full-Stack Coding Bootcamp. With a background in theatre and performing
+                <p>A Graduate of the University of North Carolina Full-Stack Coding Bootcamp. With a background in theatre and performing
                     arts, I am bringing my knowledge of entertainment into the world of development. I am seeking a job
-                    in front-end and back-end web development. I have a lengthy job history as a manager/supervisor of teams
+                    in full stack web development. I have a lengthy job history as a manager/supervisor of teams
                     of up to 25 people.
                     In my years of managerial experience, I have honed my time management
                     and organizational skills, as well as developing strong communication
