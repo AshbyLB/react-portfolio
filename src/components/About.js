d1c540895f3e13b2248d07ba2cf11bac8fa64941
Ashby Blakely <ashbyleeblakely@gmail.com>
2021-11-01 14:54:42 -0400

    Added skills to the about me

--- src/components/About.js
+++ src/components/About.js
@@ -7,20 +7,27 @@ function About() {
     return (
         <section>
             <div id="aboutStyle">
-            <h2>About Me</h2>
-            <img id="me" src={Ashby} alt="Photo of Ashby in Ireland"/>
-            <p>I'm a current student of the UNCC Full-Stack Coding Bootcamp. With a background in theatre and performing
-                arts, I am bringing my knowledge of entertainment into the world of development. I am seeking a job
-                in front-end and back-end web development. I have a lengthy job history as a manager/supervisor of teams
-                of up to 25 people.
-                In my years of managerial experience, I have honed my time management
-                and organizational skills, as well as developing strong communication
-                skills. I look
-                forward to starting my career in web development.
-                Please take a moment
-                to navigate through my portfolio page and feel free to contact me anytime.
+                <h2>About Me</h2>
+                <img id="me" src={Ashby} alt="Photo of Ashby in Ireland" />
+                <p>I'm a current student of the UNCC Full-Stack Coding Bootcamp. With a background in theatre and performing
+                    arts, I am bringing my knowledge of entertainment into the world of development. I am seeking a job
+                    in front-end and back-end web development. I have a lengthy job history as a manager/supervisor of teams
+                    of up to 25 people.
+                    In my years of managerial experience, I have honed my time management
+                    and organizational skills, as well as developing strong communication
+                    skills. I look
+                    forward to starting my career in web development.
+                    Please take a moment
+                    to navigate through my portfolio page and feel free to contact me anytime.
                 </p>
-                </div>
+                <h2>Technical Skills</h2>
+                <p>
+                    Languages: SQL, JavaScript, React, MERN, HTML/CSS, CSS, JSON, Express, Express Handlebars, Express Sessions, NodeJs, Sequelize, JQuery.
+                    Database: MongoDB, MySQL
+                    Other: Heroku, GitHub.
+                </p>
+            </div>
+
         </section>
     )
 };
