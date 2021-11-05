1645ae58f101d35a7104a8525e6f343beb2a3d3b
Ashby Blakely <ashbyleeblakely@gmail.com>
2021-11-05 11:40:16 -0400

    new icon for main

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ function Footer() {
                 <i class="fab fa-linkedin fa-4x"></i>
                 </a>
                 <a href="mailto: ashbyleeblakely@gmail.com">
-                <i class="fas fa-at fa-4x"></i>
+                <i class="fas fa-envelope-square fa-4x"></i>
                 </a>
                 <a href="https://github.com/AshbyLB" >
                 <i class="fab fa-github-square fa-4x"></i>
